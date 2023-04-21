84962b1872a6d45fbc9af0e21c966e4056a6a19f
Vitaliy <prokinvs@gmail.com>
2023-04-21 21:12:28 +0200

    feat: app component with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: '', redirectTo: 'login', pathMatch: 'full' },
+	{
+		path: 'login',
+		loadChildren: () => import('./pages/login-page/login-page.module').then((m) => m.LoginPageModule),
+	},
+	{
+		path: '**',
+		loadChildren: () => import('./pages/not-found-page/not-found-page.module').then((m) => m.NotFoundPageModule),
+	},
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
+declare const $: any;
+
 @Component({
 	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.scss'],
+	template: '<router-outlet></router-outlet>',
 })
 export class AppComponent {
 	title = 'bigland-front';
