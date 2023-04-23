dc5927b04e609780f707a5ab435349c3eb22d07d
Vitaliy <prokinvs@gmail.com>
2023-04-23 22:59:16 +0200

    refactor: ref login page

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,22 +1,67 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { AlertService } from 'src/app/core/services/alert.service';
-import { ApiService } from 'src/app/core/services/api.service';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NotificationService } from 'src/app/core/services/notifications/notification.service';
+import { LoginService } from 'src/app/core/services/security/login.service';
 
 @Component({
 	selector: 'app-login',
 	templateUrl: './login-page.component.html',
 	styleUrls: ['./login-page.component.scss'],
 })
-export class LoginPageComponent {
-	user = { login: '', password: '' };
-	constructor(private readonly alertService: AlertService) {}
+export class LoginPageComponent implements OnInit {
+	loginForm: FormGroup;
+	private loading = false;
 
-	onSubmit(f: NgForm) {
-		if (this.user.login && this.user.password) {
-			//this.apiService.requestRx(this.user)
+	constructor(
+		private formBuilder: FormBuilder,
+		private router: Router,
+		private route: ActivatedRoute,
+		private notificationService: NotificationService,
+		private loginService: LoginService,
+	) {
+		this.loginForm = this.formBuilder.group({
+			username: ['', Validators.required],
+			password: ['', Validators.required],
+		});
+	}
+
+	// eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
+	ngOnInit(): void {
+		//this.returnUrl = this.route.snapshot.queryParams['prevUrl'];
+	}
+
+	login(): void {
+		if (this.loginForm.valid) {
+			this.loading = true;
+			this.loginService.login(this.loginForm.value).subscribe(
+				() => {
+					this.onLoginSuccess();
+				},
+				(error: any) => {
+					this.onLoginFail(error);
+				},
+			);
 		} else {
-			this.alertService.notifyDangerMessage('Введите имя пользователя и пароль');
+			this.notificationService.swalError('Поле Имя пользователя и пароль обязательны для заполнения');
+		}
+	}
+
+	private onLoginSuccess(): void {
+		this.loading = false;
+		this.router.navigate(['/']).then(() => {
+			this.notificationService.success('Сеанс успешно выполнен');
+		});
+	}
+
+	private onLoginFail(error: any): void {
+		this.loading = false;
+		if (error.status === undefined) {
+			this.notificationService.error('Произошла ошибка');
+		} else if (error.status !== 0) {
+			this.notificationService.error(error.error.message);
+		} else {
+			this.notificationService.error('На данный момент нет подключения, попробуйте позже');
 		}
 	}
 }
