38190d953e871601481caa427fb8ed2ba3476774
Vitaliy <prokinvs@gmail.com>
2023-04-23 22:58:29 +0200

    feat: abstract api service with default func

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -1,17 +1,58 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Params } from '@angular/router';
+import { Observable, catchError, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { NotificationService } from './notifications/notification.service';
+import { errorNotification } from '../helpers/error-notification.helper';
 
 @Injectable()
-export class ApiService {
-	protected endpoint: string;
-	constructor(private http: HttpClient, resourcePath: string) {
-		this.endpoint = environment.apiURL + resourcePath;
+export abstract class ApiService<T> {
+	protected readonly URL: string;
+
+	protected constructor(protected httpClient: HttpClient, protected notificationService: NotificationService) {
+		this.URL = environment.URL_API + 'v2/';
 	}
 
-	requestRx(params: Params): Observable<any> {
-		return this.http.post<any>(this.endpoint, params);
+	abstract get fullUrl(): string;
+
+	create(data: T, message?: string): Observable<T> {
+		return this.httpClient.post<T>(this.fullUrl, data).pipe(
+			catchError((error) => {
+				return errorNotification(error.error, this.notificationService);
+			}),
+			tap((value: any) => {
+				this.notificationService.success(message ?? 'Se ha registrado');
+			}),
+		);
+	}
+
+	update(data: T, message?: string): Observable<T> {
+		return this.httpClient.put<T>(this.fullUrl + (data as any).id, data).pipe(
+			catchError((error) => {
+				return errorNotification(error.error, this.notificationService);
+			}),
+			tap((value: any) => {
+				this.notificationService.success(message ?? 'Se ha actualizado');
+			}),
+		);
+	}
+
+	show(id: number): Observable<T> {
+		return this.httpClient.get<T>(this.fullUrl + id).pipe(
+			catchError((error) => {
+				return errorNotification(error.error, this.notificationService);
+			}),
+		);
+	}
+
+	delete(id: number, message?: string): Observable<T> {
+		return this.httpClient.delete<T>(this.fullUrl + id).pipe(
+			catchError((error) => {
+				return errorNotification(error.error, this.notificationService);
+			}),
+			tap(() => {
+				this.notificationService.success(message ?? 'Se ha eliminado');
+			}),
+		);
 	}
 }
