073d295cf27423d1eea753c019f9d397f51949c3
Vitaliy <prokinvs@gmail.com>
2023-04-23 23:00:50 +0200

    refactor: add need module, some for test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, HttpClientModule, MatSnackBarModule], //MatSnackBarModule внести отдельно
 	providers: [],
 	bootstrap: [AppComponent],
 })
